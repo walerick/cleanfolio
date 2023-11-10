6904312455b5d99449948d8ee7833b624f2a307b
Adewale <bakareadewale24@gmail.com>
2023-11-10 09:07:08 +0100

    portfolio revised

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,10 +3,10 @@ import './Footer.css'
 const Footer = () => (
   <footer className='footer'>
     <a
-      href='https://github.com/rajshekhar26/cleanfolio'
+      href='https://github.com/walerick/cleanfolio'
       className='link footer__link'
     >
-      Created By Raj Shekhar
+      Created By Bakare Adewale
     </a>
   </footer>
 )
--- src/portfolio.js
+++ src/portfolio.js
@@ -1,71 +1,78 @@
 const header = {
-  // all the properties are optional - can be left empty or deleted
-  homepage: 'https://github.com/walerick/simple-portfolio.git',
-  title: 'JS.',
+  homepage: 'https://walerick.github.io/cleanfolio/',
+  title: 'Home',
 }
 
 const about = {
-  // all the properties are optional - can be left empty or deleted
   name: 'Bakare Adewale',
   role: 'Front End Engineer',
   description:
-    'Adipisicing sit fugit ullam unde aliquid sequi Facilis soluta facilis perspiciatis corporis nulla aspernatur. Autem eligendi rerum delectus modi quisquam? Illo ut quasi nemo ipsa cumque perspiciatis! Maiores minima consectetur.',
-  resume: 'https://example.com',
+    "Hy there!!\nI'm Adewale Bakare, a Full-stack Developer. I am passionate about my work because I believe that competence builds confidence. This motivates me always to strive for excellence in all. \n I play video games in my free time, funny enough this helps me shape the way I tackle problems in real life. \n Life is like a video-game, you need to learn how to play so you can win.",
+  resume:
+    'https://drive.google.com/open?id=1DgTylioeeCeA7YCknKYcRc55jjdY_TSQ&usp=drive_fs',
   social: {
-    linkedin: 'https://linkedin.com',
+    linkedin: 'https://www.linkedin.com/in/adewaletegar/',
     github: 'https://github.com/walerick',
   },
 }
 
 const projects = [
-  // projects can be added an removed
-  // if there are no projects, Projects section won't show up
   {
-    name: 'Project 1',
+    name: 'Cowrywise',
     description:
-      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
-    stack: ['SASS', 'TypeScript', 'React'],
-    sourceCode: 'https://github.com',
+      'Cowrywise web clone.\n - Mobile resposive.\n -Deployed in netlify.',
+    stack: ['JavaScript', 'React', 'SQL'],
+    sourceCode: 'https://github.com/Cowrywise',
+    livePreview: 'https://cowrywlse.netlify.app',
+  },
+  {
+    name: 'Movie-Box',
+    description:
+      'A movie web application that allows users to search for movies, view details about them and save their favorites.',
+    stack: ['JavaScript', 'React', 'Api'],
+    sourceCode: 'https://github.com/walerick/hng_movie_task',
+    livePreview: 'https://moviesbocks.vercel.app/',
+  },
+  {
+    name: 'Simplify',
+    description:
+      'A web application that simplifies complex sentences for users using ReactJS and OpenAI API. ',
+    stack: ['OpenAI', 'JavaScript', 'React'],
+    sourceCode: 'https://github.com/walerick/simplify',
+    livePreview: 'https://simplify-eta.vercel.app/',
+  },
+  {
+    name: 'National Population Commission',
+    description:
+      'NPC web app that allows authorised user to register and print National Certificates.\n-Added user authentication.',
+    stack: ['JavaScript', 'Java', 'SQL'],
+    sourceCode: 'https://github.com/walerick/npc-frontend',
     livePreview: 'https://github.com',
   },
   {
-    name: 'Project 2',
+    name: 'Advice-generator',
     description:
-      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
-    stack: ['SASS', 'TypeScript', 'React'],
-    sourceCode: 'https://github.com',
-    livePreview: 'https://github.com',
-  },
-  {
-    name: 'Project 3',
-    description:
-      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
-    stack: ['SASS', 'TypeScript', 'React'],
+      'An advice generator app using the Advice Slip API -Generate a new piece of advice by clicking the dice icon',
+    stack: ['html', 'CSS', 'JavaScript'],
     sourceCode: 'https://github.com',
     livePreview: 'https://github.com',
   },
 ]
 
 const skills = [
-  // skills can be added or removed
-  // if there are no skills, Skills section won't show up
   'HTML',
   'CSS',
   'JavaScript',
-  'TypeScript',
   'React',
-  'Redux',
-  'SASS',
-  'Material UI',
+  'Java',
+  'Python',
+  'Go',
   'Git',
-  'CI/CD',
-  'Jest',
-  'Enzyme',
+  'SQL',
 ]
 
 const contact = {
-  // email is optional - if left empty Contact section won't show up
-  email: 'johnsmith@mail.com',
+  email: 'bakareadewale24@mail.com',
 }
 
 export { header, about, projects, skills, contact }
